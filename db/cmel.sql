9d575d541fcc3bd1d1296f086954cd28d3ece1d6
Rukusu <nesesitounos@hotmail.com>
2018-02-27 12:57:25 -0600

    Added DB dump

---
+++ db/cmel.sql
@@ -0,0 +1,71 @@
+-- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: cmel
+-- ------------------------------------------------------
+-- Server version	10.1.26-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `participante`
+--
+
+DROP TABLE IF EXISTS `participante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `participante` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `NOMBRE` varchar(300) NOT NULL,
+  `INSTITUCION` varchar(140) NOT NULL,
+  `EMAIL` varchar(140) NOT NULL,
+  `REGION_FK` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_PART_REG` (`REGION_FK`),
+  CONSTRAINT `FK_PART_REG` FOREIGN KEY (`REGION_FK`) REFERENCES `regiones` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `regiones`
+--
+
+DROP TABLE IF EXISTS `regiones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `regiones` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `NOMBRE` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `regiones`
+--
+
+LOCK TABLES `regiones` WRITE;
+/*!40000 ALTER TABLE `regiones` DISABLE KEYS */;
+INSERT INTO `regiones` VALUES (1,'PARC'),(2,'RELAN'),(3,'RELAF'),(4,'RELEM'),(5,'RELAL');
+/*!40000 ALTER TABLE `regiones` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-27 12:49:34
